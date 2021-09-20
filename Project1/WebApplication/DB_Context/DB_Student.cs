167435b0745499274463200daff96d892d19c720
s9904320 <91059581+s9904320@users.noreply.github.com>
2021-09-20 22:59:51 +0800

    資料庫連結
    資料庫連結

---
+++ Project1/WebApplication/DB_Context/DB_Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication.DB_Context
+{
+    public class DB_Student : DbContext
+    {
+        public DB_Student(DbContextOptions<DB_Student> options) : base(options) { }
+    }
+}
