54b8b4f682f9d3e27ac0dba773045570e5e39bcf
s9904320 <91059581+s9904320@users.noreply.github.com>
2021-09-20 23:10:14 +0800

    model建立
    model建立

--- Project1/WebApplication/DB_Context/DB_Student.cs
+++ Project1/WebApplication/DB_Context/DB_Student.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using WebApplication.Models;
 
 namespace WebApplication.DB_Context
 {
     public class DB_Student : DbContext
     {
         public DB_Student(DbContextOptions<DB_Student> options) : base(options) { }
+        public DbSet<StudentInfoT> StudentInfo { get; set; }
     }
 }
---
+++ Project1/WebApplication/Models/StudentInfoT.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.Models
+{
+    public class StudentInfoT
+    {
+        [Key]
+        public int recno { get; set; }
+        public string? name { get; set; }
+        public bool sex { get; set; }
+        public int number { get; set; }
+    }
+}
